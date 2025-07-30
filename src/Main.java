78e427aba541f6e4e0f2bd1aa87a8148e043ffd3
gbmao <gabriel.machado.oli@gmail.com>
2025-07-30 19:20:22 -0300

    feat: just a change to better read the new string output

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args){
 
 
-    System.out.println("The location of the number: " + "10" + " is: " + SearchUtils.search(Data.numbers, 120));
+    System.out.println(SearchUtils.search(Data.numbers, 20));
 
     }
 
