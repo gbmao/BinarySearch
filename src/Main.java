de9c7a186d91db01dc9a31a7c64978c55725306f
gbmao <gabriel.machado.oli@gmail.com>
2025-07-30 11:15:08 -0300

    wip: add a way to use the search

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args){
 
-        System.out.println("Hello World");
+
+    System.out.println("The location of the number: " + "10" + " is: " + SearchUtils.search(Data.numbers, 120));
+
     }
 
 }
